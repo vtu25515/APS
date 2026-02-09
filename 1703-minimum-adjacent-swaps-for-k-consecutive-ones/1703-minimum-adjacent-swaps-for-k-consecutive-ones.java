89469417d0b42cd72cc60e067219be21fb8a82ad
vtu25515 <vtu25515@veltech.edu.in>
2026-02-09 09:24:55 +0530

    Time: 13 ms (52.05%), Space: 107.4 MB (53.42%) - LeetHub

---
+++ 1703-minimum-adjacent-swaps-for-k-consecutive-ones/1703-minimum-adjacent-swaps-for-k-consecutive-ones.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int minMoves(int[] nums, int k) {
+      
+        List<Integer> ones = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                ones.add(i);
+            }
+        }
+        int prefSum[]=new int[ones.size()];
+        prefSum[0] =ones.get(0);
+        for(int i = 1; i < ones.size(); i++)
+            prefSum[i] = prefSum[i - 1] + ones.get(i);
+
+        
+        int minMoves = Integer.MAX_VALUE;
+        int left = 0;
+        int right = k - 1;
+        while(right<ones.size()){
+            if(k%2==0){
+                int mid = (left + right) / 2;
+                int radius=mid-left;
+                int excessMoves;int total=0;
+               int lsum=(mid == 0 ? 0 : prefSum[mid-1]) - (left == 0 ? 0 : prefSum[left-1]);
+              int rsum=prefSum[right]-prefSum[mid] ;
+               excessMoves=radius*(radius+1)+(radius+1);
+               total=rsum-lsum-ones.get(mid)-(excessMoves);
+               minMoves=Math.min(minMoves,total);
+            }
+            else{
+                int mid = (left + right) / 2;
+                int radius=mid-left;
+                int excessMoves;int total=0;
+               int lsum=(mid == 0 ? 0 : prefSum[mid - 1]) - (left == 0 ? 0 : prefSum[left - 1]);
+              int rsum=prefSum[right]-prefSum[mid] ;
+               excessMoves=radius*(radius+1);
+               total=rsum-lsum-(excessMoves);
+                minMoves=Math.min(minMoves,total);
+            }
+            left+=1;
+            right+=1;
+        }
+       
+   return minMoves;
+    }
+}
